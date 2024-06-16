e1a9e19bad94d4d6808a58e7307e165a8b16fb67
Nguyễn Đức Việt <116934756+vietnguyen20139@users.noreply.github.com>
2024-06-16 14:38:58 +0700

    web

---
+++ main_page/js_file/ui_global_var.js
@@ -0,0 +1,7 @@
+// get element btn submit, export to use var global
+// export const txtPassword = document.querySelector('#btnSignin')
+export const btnSignin = document.getElementById('btnSignin')
+
+export const btnSignup = document.getElementById('btnSignup')
+export const btnResetPass = document.getElementById('btnResetPass')
+export const nameForPath = "";
