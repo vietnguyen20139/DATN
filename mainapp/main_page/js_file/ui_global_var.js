c036ecaa9bddadc95e70301135da2afb081e1ee6
Nguyễn Đức Việt <116934756+vietnguyen20139@users.noreply.github.com>
2024-06-16 14:37:55 +0700

    Delete mainapp/main_page directory

--- mainapp/main_page/js_file/ui_global_var.js
+++
@@ -1,7 +0,0 @@
-// get element btn submit, export to use var global
-// export const txtPassword = document.querySelector('#btnSignin')
-export const btnSignin = document.getElementById('btnSignin')
-
-export const btnSignup = document.getElementById('btnSignup')
-export const btnResetPass = document.getElementById('btnResetPass')
-export const nameForPath = "";
